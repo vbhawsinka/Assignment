5ca09e33815aa601c6f7dd650c9d26eb136a410d
Vivek Bhawsinka <97597092+vbhawsinka@users.noreply.github.com>
2024-07-17 18:25:04 +0530

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector(".nav"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
